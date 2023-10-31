5ab6955d457e0b49028a4fdf5327d8f8e3d42f59
patriciovenero <patricio.venero@tecsup.edu.pe>
2023-10-31 13:26:17 -0500

    proyecto completa se agrego algunas implementaciones como el lislado de mis grabaciones ademas de usar el editingstyle

--- SoundBoard/ViewController.swift
+++ SoundBoard/ViewController.swift
@@ -6,12 +6,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
 
     @IBOutlet weak var tablaGrabaciones: UITableView!
     var grabaciones : [Grabacion] = []
+    var reproducirAudio: AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,7 +30,35 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.textLabel?.text = grabacion.nombre
         return cell
     }
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do{
+            grabaciones = try
+            context.fetch(Grabacion.fetchRequest())
+            tablaGrabaciones.reloadData()
+        }catch{}
+    }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let grabacion = grabaciones[indexPath.row]
+        do {
+            reproducirAudio = try AVAudioPlayer(data: grabacion.audio! as Data)
+            reproducirAudio?.play()
+        }catch{}
+        tablaGrabaciones.deselectRow(at: indexPath, animated: true)
+    }
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            let grabacion = grabaciones[indexPath.row]
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            context.delete(grabacion)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            do{
+                grabaciones = try context.fetch(Grabacion.fetchRequest())
+                tablaGrabaciones.reloadData()
+            }catch{}
+        }
+    }
 
 
 }
