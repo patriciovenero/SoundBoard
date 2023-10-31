ec8a8e7cfd5df8bf8d01516df27599d836f44552
patriciovenero <patricio.venero@tecsup.edu.pe>
2023-10-31 10:52:42 -0500

    punto 61

--- SoundBoard/ViewController.swift
+++ SoundBoard/ViewController.swift
@@ -7,12 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+
+    @IBOutlet weak var tablaGrabaciones: UITableView!
+    var grabaciones : [Grabacion] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tablaGrabaciones.dataSource = self
+        tablaGrabaciones.delegate = self
     }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return grabaciones.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let grabacion = grabaciones[indexPath.row]
+        cell.textLabel?.text = grabacion.nombre
+        return cell
+    }
+    
 
 
 }
